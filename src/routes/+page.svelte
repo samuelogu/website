62892ab11293962d0a7174652f0e9beac486de94
Samuel Ogu <hello@samuelogu.com>
2022-10-01 02:22:34 +0100

    coming soon update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,40 @@
 <center>
     <h1 style="margin-top: 100px;">Coming soon...</h1>
-    <p>I'm currently building my website</p>
+    <p style="margin-bottom: 30px">I'm currently building my website</p>
+    <div>
+        <img class="avatar" src="/assets/images/profile.JPG" alt="Picture of Samuel Ogu">
+        <ul>
+            <li><a href="https://github.com/samuelogu" target="_blank">Github</a></li>
+            <li><a href="https://instagram.com/samuel_ogu" target="_blank">Instagram</a></li>
+            <li><a href="https://twitter.com/samuel_ogu" target="_blank" style="margin-right: 0;">Twitter</a></li>
+        </ul>
+    </div>
 </center>
 
 <style>
-    h1, p {
+    h1, p, a {
         color: #e9edf0;
         font-family: Inter,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
+    }
+    ul, li {
+        display: contents;
+    }
+    p {
+        font-size: 18px;
+    }
+    a:link {
+        margin-right: 20px;
+    }
+    .avatar {
+        height: 150px;
+        width: 150px;
+        border-radius: 50%;
+        transform: rotate(0deg);
+        transition: transform .5s ease-in-out;
+        object-fit: cover;
+        display: block;
+        object-position: top;
+        border: 1px solid #0fb7fe;
+        margin-bottom: 20px;
     }
 </style>
