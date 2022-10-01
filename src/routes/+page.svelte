fb16d33b56940e9c482bc5c3d8e6c272a04793ed
Samuel Ogu <hello@samuelogu.com>
2022-10-01 01:58:33 +0100

    coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Samuel Ogu</h1>
+<center>
+    <h1 style="margin-top: 100px;">Coming soon...</h1>
+    <p>I'm currently building my website</p>
+</center>
+
+<style>
+    h1, p {
+        color: #e9edf0;
+        font-family: Inter,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji;
+    }
+</style>
